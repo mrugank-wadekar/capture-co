e2cd13d31332886c54fbc547044151297c1343ee
mrugank-wadekar <mrugank9009@gmail.com>
2023-03-01 19:29:58 +0530

    Add Logo Card effect

--- mystyles.js
+++ mystyles.js
@@ -15,3 +15,26 @@ images.forEach((image) => {
     }
   });
 });
+
+const logo = document.querySelector('.logo');
+const fixedInner1 = document.querySelector('.fixed-inner-1');
+const leftDiv =document.querySelector('.left');
+const rightDiv =document.querySelector('.right');
+
+logo.addEventListener('mouseover', () => {
+  fixedInner1.style.width = '80%';
+  fixedInner1.style.height = '64%';
+  fixedInner1.style.borderRadius ='14px';
+  leftDiv.style.opacity='1';
+  rightDiv.style.opacity='1';
+  logo.style.opacity='0';
+});
+
+// logo.addEventListener('mouseout', () => {
+//   fixedInner1.style.width = '25%';
+//   fixedInner1.style.height = '44%';
+//   fixedInner1.style.borderRadius ='50%';
+//   leftDiv.style.opacity='0';
+//   rightDiv.style.opacity='0';
+//   logo.style.opacity='1';
+// });
